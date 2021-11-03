a0ea77966b588b7a09624a3eb5f59ebd7f509ecd
ckachok <mefelion89@gmail.com>
2021-11-04 01:50:33 +0300

    feat: add functions to create a new directory and get the files of the current directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,18 @@
+const { mkdir, readdir, copyFile } = require('fs/promises');
+const path = require('path');
 
+const pathCurrentFolder = path.join(__dirname, 'files');
+const pathCopiedFolder = path.join(__dirname, 'files-copy');
+
+const createDirectory = async () => {
+  await mkdir(pathCopiedFolder, { recursive: true })
+}
+
+const getFiles = async () => {
+  const folderContents = await readdir(pathCurrentFolder, { withFileTypes: true });
+  folderContents.forEach(file => {
+    if (file.isFile()) {
+      copyFileToDirectory(file.name);
+    }
+  })
+}
