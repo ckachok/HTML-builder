1f0709a6abbee6055e6650674a843ff3e6ec29ab
ckachok <mefelion89@gmail.com>
2021-11-08 00:06:58 +0300

    feat: add the funtion for creating the bundle styles

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,28 +1,29 @@
 const { readdir, mkdir, rmdir } = require('fs/promises');
 const path = require('path');
 const fs = require('fs');
-// const os = require('os');
+const os = require('os');
 
 const pathProjectFolder = path.join(__dirname, 'project-dist');
 const pathComponentsFolder = path.join(__dirname, 'components');
+const pathStylesFolder = path.join(__dirname, 'styles');
 const pathTemplate = path.join(__dirname, 'template.html');
 // const arrStyles = [];
 
-const createDirectory = async () => {
-  await mkdir(pathProjectFolder, { recursive: true });
+const createDirectory = async (pathFolder) => {
+  await mkdir(pathFolder, { recursive: true });
 }
 
-const deleteDirectory = async () => {
-  await rmdir(pathProjectFolder, { recursive: true });
+const deleteDirectory = async (pathFolder) => {
+  await rmdir(pathFolder, { recursive: true });
 }
 
-const getComponentFiles = async () => {
-  const componentsFolderFiles = await readdir(pathComponentsFolder, { withFileTypes: true });
+const getFiles = async (pathFolderFiles, extension) => {
+  const folderFiles = await readdir(pathFolderFiles, { withFileTypes: true });
 
-  return componentsFolderFiles.filter(file => {
+  return folderFiles.filter(file => {
     const extName = path.extname(file.name).slice(1);
     
-    if (file.isFile() && extName === 'html') {
+    if (file.isFile() && extName === extension) {
       return file;
     };
   });
@@ -43,7 +44,7 @@ const getHtmlTemplate = async () => {
 }
 
 const createBundleHtml = async () => {
-  const components = await getComponentFiles();
+  const components = await getFiles(pathComponentsFolder, 'html');
   let htmlBundle = await getHtmlTemplate();
 
   return Promise.all(components.map(component => 
@@ -64,12 +65,46 @@ const createBundleHtml = async () => {
   )).then(() => htmlBundle);
 }
 
+const writeBundleHtml = (html) => {
+  let writeableStream = fs.createWriteStream(path.join(pathProjectFolder, 'index.html'));
+  writeableStream.write(html);
+}
+
+const createBundleStyles = async () => {
+  const styleFiles = await getFiles(pathStylesFolder, 'css');
+  let styleBundle = '';
+
+  return Promise.all(styleFiles.map(styleFile => 
+    new Promise((res, rej) => {
+      let styles = '';
+      const pathStyleFile = path.join(pathStylesFolder, styleFile.name);
+      const styleFileReadStream = fs.createReadStream(pathStyleFile);
+
+      styleFileReadStream.on('data', data => {
+        styles += data;
+      });
+      styleFileReadStream.on('end', () => {
+        styleBundle = styleBundle + styles + os.EOL;
+        res(styleBundle);
+      });
+    })
+  )).then(() => styleBundle);
+}
+
+const writeBundleStyles = (style) => {
+  const writeableStream = fs.createWriteStream(path.join(pathProjectFolder, 'style.css'));
+  writeableStream.write(style);
+}
 
 const init = async () => {
-  await createDirectory();
-  let writeableStream = fs.createWriteStream(path.join(pathProjectFolder, 'index.html'))
+  await deleteDirectory(pathProjectFolder);
+  await createDirectory(pathProjectFolder);
+
   const htmlBundle = await createBundleHtml();
-  writeableStream.write(htmlBundle);
+  writeBundleHtml(htmlBundle);
+
+  const stylesBundle = await createBundleStyles();
+  writeBundleStyles(stylesBundle);
 }
 
 init()
