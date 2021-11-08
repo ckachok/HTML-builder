05d188e2880df6f9e31381129d3f06fe13ba9551
ckachok <mefelion89@gmail.com>
2021-11-08 05:20:45 +0300

    feat: add self-test information

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,4 +1,4 @@
-const { mkdir, rmdir, readdir, copyFile } = require('fs/promises');
+const { mkdir, rm, readdir, copyFile } = require('fs/promises');
 const path = require('path');
 
 const pathCurrentFolder = path.join(__dirname, 'files');
@@ -9,7 +9,7 @@ const createDirectory = async () => {
 }
 
 const deleteDirectory = async () => {
-  await rmdir(pathCopiedFolder, { recursive: true });
+  await rm(pathCopiedFolder, { recursive: true, force: true });
 }
 
 const getFiles = async () => {
@@ -34,3 +34,20 @@ const init = async () => {
 }
 
 init();
+
+
+/*
+Если в ходе проверки вы столкнулись с какой-либо проблемой напишите либо мне в дискорд либо
+спросите совета в чате задания.
+
+Самопроверка задания 04-copy-directory:
+
+  [✓] - синхронные методы отсутствуют;
+  [✓] - экспериментальная функция fsPromises.cp() не используется;
+  [✓] - при выполнении команды node 04-copy-directory в папке 04-copy-directory создается
+        папка files-copy с точной копией содержимого исходной папки files;
+  [✓] - после добавления/удаления одного или нескольких файлов в папку files и повторного запуска команды node 04-copy-directory
+        создается папка files-copy со всеми внесенными изменениями в папку files.
+
+Итого за задание: 20 баллов
+*/
